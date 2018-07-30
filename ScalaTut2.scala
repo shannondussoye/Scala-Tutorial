eb449b3fd1d205f45291a92d13f8436d75a6754f
Shannon Dussoye <shannonduss@live.com>
2018-07-30 22:53:54 +1000

    adding codes

---
+++ ScalaTut2.scala
@@ -0,0 +1,32 @@
+object ScalaTut1 {
+	def main(args: Array[String]) {
+	
+    // For loop extended
+	var i = 0
+	var randLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for (i <- 0 until randLetters.length){
+    	println(randLetters(i))
+    }
+
+    //List in Scala
+    var aList = List(1,2,3,4,5)
+    for(i <- aList) {
+      println("List Items "+i)
+    }
+
+	// Loop with Condition
+	var evenList = for { i <- 1 to 20
+		if (i % 2) == 0
+		} yield i
+
+	for(i<- evenList){
+		println(i)
+	}
+
+	// nested Loop
+	for(i <- 1 to 5; j <- 6 to 10){
+		println("i: "+i)
+		println("j: "+j)
+	}
+  }
+}
---
+++ template.scala
@@ -0,0 +1,5 @@
+object ScalaTut1 {
+	def main(args: Array[String]) {
+		// Code here
+	}
+}
